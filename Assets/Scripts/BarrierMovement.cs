4960ac64c473b3939cc60f151c3707e156a50fb0
Zhaeong <owen_zzz@hotmail.com>
2016-01-16 12:21:25 -0500

    added barrier movement

---
+++ Assets/Scripts/BarrierMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrierMovement : MonoBehaviour {
+    public float speed = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate() {
+        Vector3 vNewPosition = transform.position;
+        vNewPosition.y -= 0.001f;
+        transform.Translate(Time.deltaTime * speed, 0, 0);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
