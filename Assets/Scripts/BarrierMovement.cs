1c3d4880e672cfbc187757cfd8377b27a89e4eba
Zhaeong <owen_zzz@hotmail.com>
2016-01-16 14:51:20 -0500

    changed camera angles and other stuff

--- Assets/Scripts/BarrierMovement.cs
+++ Assets/Scripts/BarrierMovement.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class BarrierMovement : MonoBehaviour {
     public float speed = 5;
     public int scoreCounter;
+    public Text scoreText;
 
     // Use this for initialization
     void Start () {
         scoreCounter = 0;
+        scoreText.text = "Score:" + scoreCounter.ToString();
 	}
 
     void FixedUpdate() {
@@ -24,11 +27,12 @@ public class BarrierMovement : MonoBehaviour {
         if (other.CompareTag("EndWall"))
         {
             float fSize = Random.Range(7.0f, 18.0f);            
-            Vector3 vStartPos = new Vector3(-10, fSize, 0);
+            Vector3 vStartPos = new Vector3(-20, fSize, 0);
             transform.position = vStartPos;           
             scoreCounter += 10;
             Debug.Log("Collided ");
             Debug.Log(scoreCounter);
+            scoreText.text = "Score:" + scoreCounter.ToString();
         }
     }
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour {
     public GameObject PlayerChar;
     public float cameraXval = 10;
     public float cameraYval = 5;
+    public float cameraZval = 5;
     // Use this for initialization
     void Start () {
 	
@@ -16,7 +17,7 @@ public class CameraController : MonoBehaviour {
         Vector3 playerPosit = PlayerChar.transform.position;
         float cameraX = PlayerChar.transform.position.x + cameraXval;
         float cameraY = PlayerChar.transform.position.y + cameraYval;
-        float cameraZ = PlayerChar.transform.position.z;
+        float cameraZ = PlayerChar.transform.position.z + cameraZval;
         transform.position = new Vector3(cameraX, cameraY, cameraZ);
         transform.LookAt(playerPosit);
 
