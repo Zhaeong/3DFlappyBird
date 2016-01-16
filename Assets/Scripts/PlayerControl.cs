39278418b016471a49e141c1426a25a398db04b4
Zhaeong <owen_zzz@hotmail.com>
2016-01-16 11:26:22 -0500

    added basic files

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    private Rigidbody rb;
+    public float jump_height = 4;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate() {
+        float jump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(0, jump_height * jump, 0);
+        rb.AddForce(movement);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
